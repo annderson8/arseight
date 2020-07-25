811cd17506f93f5c4aaee3291918e9624e455169
Annderson Rey <anndersonrey@gmail.com>
2020-07-25 15:32:42 -0500

    Modify tracking id google

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,7 +38,7 @@ module.exports = {
             resolve: `gatsby-plugin-google-analytics`,
             options: {
                 // The property ID; the tracking code won't be generated without it
-                trackingId: `G-YL8EMRNFJ6`,
+                trackingId: `UA-171061339-1`,
                 // Defines where to place the tracking script - `true` in the head and `false` in the body
                 head: false,
             },
